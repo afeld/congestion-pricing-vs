4853a0e40d2cc69f933e1920afdb09354b89eced
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-03 22:43:47 -0500

    sum the vehicle entrances

--- main.py
+++ main.py
@@ -10,22 +10,30 @@ def get_entrances():
     # https://data.ny.gov/Transportation/MTA-Congestion-Relief-Zone-Vehicle-Entries-Beginni/t6yz-b64h/about_data
     params = urlencode(
         {
-            "$select": "date_extract_woy(toll_date) AS week, SUM(crz_entries) AS count",
+            "$select": "date_extract_woy(toll_date) AS week, SUM(crz_entries) AS count_included, SUM(excluded_roadway_entries) AS count_excluded",
             "$group": "week",
         }
     )
-    return pd.read_csv(f"https://data.ny.gov/resource/t6yz-b64h.csv?{params}")
+    entrances = pd.read_csv(f"https://data.ny.gov/resource/t6yz-b64h.csv?{params}")
+
+    entrances["count"] = entrances["count_included"] + entrances["count_excluded"]
+
+    return entrances
 
 
-st.title("MTA Congestion Relief Zone Vehicle Entries")
+def run():
+    st.title("MTA Congestion Relief Zone Vehicle Entries")
 
-entrances = get_entrances()
-entrances
+    entrances = get_entrances()
+    entrances
 
-fig = px.line(
-    entrances,
-    x="week",
-    y="count",
-    title="Vehicle entries by week of the year",
-)
-st.plotly_chart(fig)
+    fig = px.line(
+        entrances,
+        x="week",
+        y="count",
+        title="Vehicle entries by week of the year",
+    )
+    st.plotly_chart(fig)
+
+
+run()
