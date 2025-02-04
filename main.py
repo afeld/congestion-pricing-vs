f39e71ae6b0f4fd700f8ad192aa7325d0874dd29
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-03 22:38:52 -0500

    improve the streamlit display

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-params = urlencode(
-    {
-        "$select": "date_extract_woy(toll_date) AS week, SUM(crz_entries) AS count",
-        "$group": "week",
-    }
-)
-# https://data.ny.gov/Transportation/MTA-Congestion-Relief-Zone-Vehicle-Entries-Beginni/t6yz-b64h/about_data
-entrances = pd.read_csv(f"https://data.ny.gov/resource/t6yz-b64h.csv?{params}")
-st.dataframe(entrances)
+
+@st.cache_data
+def get_entrances():
+    # https://data.ny.gov/Transportation/MTA-Congestion-Relief-Zone-Vehicle-Entries-Beginni/t6yz-b64h/about_data
+    params = urlencode(
+        {
+            "$select": "date_extract_woy(toll_date) AS week, SUM(crz_entries) AS count",
+            "$group": "week",
+        }
+    )
+    return pd.read_csv(f"https://data.ny.gov/resource/t6yz-b64h.csv?{params}")
+
+
+st.title("MTA Congestion Relief Zone Vehicle Entries")
+
+entrances = get_entrances()
+entrances
 
 fig = px.line(
     entrances,
@@ -20,4 +28,4 @@ fig = px.line(
     y="count",
     title="Vehicle entries by week of the year",
 )
-fig.show()
+st.plotly_chart(fig)
