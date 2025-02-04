93a1e9eb11ea0aa6ca11136466a0812365302e90
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-03 22:10:17 -0500

    display a chart

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import plotly.express as px
+import streamlit as st
 
 # https://data.ny.gov/Transportation/MTA-Congestion-Relief-Zone-Vehicle-Entries-Beginni/t6yz-b64h/about_data
 entrances = pd.read_csv("https://data.ny.gov/resource/t6yz-b64h.csv")
-entrances
+st.dataframe(entrances)
+
+fig = px.histogram(
+    entrances,
+    x="vehicle_class",
+    title="Vehicle types entering the MTA Congestion Relief Zone",
+)
+fig.show()
