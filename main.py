154f8e2cbe898f527f37eb59fec26ceeda48e629
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-03 22:27:22 -0500

    show a line chart of congestion pricing entrances over time

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+from urllib.parse import urlencode
+
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+params = urlencode(
+    {
+        "$select": "toll_date, SUM(crz_entries) AS count",
+        "$group": "toll_date",
+    }
+)
 # https://data.ny.gov/Transportation/MTA-Congestion-Relief-Zone-Vehicle-Entries-Beginni/t6yz-b64h/about_data
-entrances = pd.read_csv("https://data.ny.gov/resource/t6yz-b64h.csv")
+entrances = pd.read_csv(f"https://data.ny.gov/resource/t6yz-b64h.csv?{params}")
 st.dataframe(entrances)
 
-fig = px.histogram(
+fig = px.line(
     entrances,
-    x="vehicle_class",
-    title="Vehicle types entering the MTA Congestion Relief Zone",
+    x="toll_date",
+    y="count",
+    title="Vehicle entries by date",
 )
 fig.show()
